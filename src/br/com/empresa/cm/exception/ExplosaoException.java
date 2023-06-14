f3e831259024abc9be6441f628f6b40f35f0d8a2
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2023-06-14 20:11:12 -0300

    Classe Campo

---
+++ src/br/com/empresa/cm/exception/ExplosaoException.java
@@ -0,0 +1,7 @@
+package br.com.empresa.cm.exception;
+
+public class ExplosaoException extends RuntimeException {
+
+	private static final long serialVersionUID = -2080687783682156278L;
+
+}
---
+++ src/br/com/empresa/cm/model/Tabuleiro.java
@@ -0,0 +1,5 @@
+package br.com.empresa.cm.model;
+
+public class Tabuleiro {
+	
+}
